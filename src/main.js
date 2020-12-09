f4266258771d0ef8168eca49d70da1d4ec87725f
Nikita <64955751+Nikita8121@users.noreply.github.com>
2020-12-09 23:16:42 +0300

    date and navbar

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
+import dateFilter from '@/filters/date.filter';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+
 import 'materialize-css/dist/js/materialize.min';
 
 Vue.config.productionTip = false;
+
+Vue.filter('date', dateFilter);
 
 new Vue({
   router,
